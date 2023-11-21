d17fd21e5d684be29b938c08cd23e8b5501b8ddc
Rohit Kumar Singh <rohitkumarsinghatwork@gmail.com>
2023-11-21 14:44:22 +0530

    Publish Repo

---
+++ SpringBootDemoProject1/src/main/java/com/springbootdemoproject1/SpringBootDemoProject1Application.java
@@ -0,0 +1,13 @@
+package com.springbootdemoproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoProject1Application.class, args);
+	}
+
+}
